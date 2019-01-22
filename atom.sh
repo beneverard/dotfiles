f699b5fef4c2c12ff16c5d9c7630e2399d01ac2f
Ben Everard <hello@beneverard.co.uk>
2019-01-22 21:21:03 +0000

    Added atom package installer

---
+++ atom.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apm install atom-material-ui
+apm install bracket-colorizer
+apm install busy-signal
+apm install docblockr
+apm install file-icons
+apm install goto-definition
+apm install intentions
+apm install language-vue
+apm install less-than-slash
+apm install linter-ui-default
+apm install minimap
+apm install minimap-git-diff
+apm install pigments
+apm install tabs-to-spaces
