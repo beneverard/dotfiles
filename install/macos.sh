6fe0a85fdcccd9dcfedd99364ddaac4184a44ece
Ben Everard <hello@beneverard.co.uk>
2019-04-07 20:55:33 +0100

    Removed spectacle config

--- install/macos.sh
+++ install/macos.sh
@@ -731,13 +731,6 @@ defaults write com.operasoftware.OperaDeveloper PMPrintingExpandedStateForPrint2
 # defaults write com.irradiatedsoftware.SizeUp ShowPrefsOnNextStart -bool false
 
 ###############################################################################
-# Spectacle.app                                                               #
-###############################################################################
-
-# Set up my preferred keyboard shortcuts
-# cp -r init/spectacle.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
-
-###############################################################################
 # Transmission.app                                                            #
 ###############################################################################
 
