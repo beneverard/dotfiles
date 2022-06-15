90cd4dc9ae2bdd60cbc87719f5874a70658b9016
Ben Everard <ben@theideabureau.co>
2022-06-15 09:47:07 +0100

    Added Ray cask install

--- install/brew.sh
+++ install/brew.sh
@@ -117,6 +117,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	brew install --cask iterm2
 	brew install --cask notion
 	brew install --cask pocket-casts
+	brew install --cask ray
 	brew install --cask sequel-pro
 	brew install --cask sketch
 	brew install --cask slack
