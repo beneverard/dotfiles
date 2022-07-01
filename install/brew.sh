5d340db6b4879cb60c12663108624e8a32d22a0b
Ben Everard <ben@theideabureau.co>
2022-07-01 16:30:37 +0100

    Changed sequel pro → sequel ace

--- install/brew.sh
+++ install/brew.sh
@@ -122,7 +122,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	brew install --cask openvpn-connect
 	brew install --cask pocket-casts
 	brew install --cask ray
-	brew install --cask sequel-pro
+	brew install --cask sequel-ace
 	brew install --cask sketch
 	brew install --cask slack
 	brew install --cask spotify
