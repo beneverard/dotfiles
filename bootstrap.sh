9abca3c7b1bc116d0441420152bbad460197e98c
Ben Everard <ben@theideabureau.co>
2024-09-14 18:51:39 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin main;
 
 function doIt() {
-	rsync -avh --no-perms ./dots/.[^.]* ~;
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
